c284f27d6bf70955d8d64cbca3c2e5b32cc563d0
AliceHuang1027 <aliceladygo@gmail.com>
2020-05-09 02:40:35 -0700

    server.js

---
+++ server.js
@@ -0,0 +1,83 @@
+//requires all the dependencies
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const {v4:uuid} = require('uuid')
+const port = process.env.PORT || 8000
+
+let todos = []
+fs.readFile('./notes',(err,data)=>{
+    if(err) {
+        // this means the file does not exsit
+        return console.log('error reading file')}
+        const str = data.toString()
+        if(str){
+            todos = JSON.parse(str)
+        }
+})
+
+app.use(express.static(__dirname +'/public'))
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.options('/todolist/*', (req, res) => {
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header('Access-Control-Allow-Origin', req.headers.origin)
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE')
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Credentials'
+    )
+    res.send('ok')
+  })
+  
+
+app.get("/todolist", (req, res)=>{
+    res.sendFile("index.html",{root:__dirname})
+})
+app.get("/todolist/todos",(req,res)=>{
+    fs.writeFile('./notes',JSON.stringify(todos),()=>{})
+    res.json(todos)
+
+})
+
+app.post("/todolist/todos",(req,res)=>{
+    if(!req.body.text){
+        return res.status(400).send("please provide a todo item.")
+    }
+    const $id = uuid()
+    todos.push({"id":$id,"complete":false,"text":req.body.text,"createdAt":Date.now()})
+    
+    res.sendFile("index.html",{root:__dirname})
+})
+app.patch("/todolist/todos/:id",(req,res)=>{
+    const todoId = req.params.id
+    const item = todos.find((e)=>{
+        return e.id===todoId
+    })
+    if(req.body.complete!=item.complete){
+       return todos.forEach((e)=>{
+            if(e.id===todoId){
+                e.complete=req.body.complete
+            }
+        })
+    }
+    if(req.body.text){
+        return todos.forEach((e)=>{
+            if(e.id===todoId){
+                e.text=req.body.text
+            }
+        })
+    }
+    res.sendFile("index.html",{root:__dirname})
+})
+
+app.delete("/todolist/todos/:id",(req,res)=>{
+    const todoId = req.params.id
+    
+    todos = todos.filter((e)=>{
+        return e.id !==todoId
+    })
+    res.sendFile("index.html",{root:__dirname})
+})
+
+app.listen(port,()=>{console.log(`listening on port ${port} `)})
